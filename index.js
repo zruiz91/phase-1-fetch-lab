64835021091b77f2d18d6fff13fa8ff409904865
Zach Ruiz <powersbooth10@gmail.com>
2022-09-06 10:48:35 -0500

    commit tree 138b7062cd8a2cbfb95bfa15735b77021cd6a678

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(function (res) {
+    return res.json()
+  })
+  .then(function (data) {
+    return renderBooks(data)
+  })
+  
+  
   
 }
 
